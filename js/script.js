1391479dea3c336407690e647bdf19e9eef3800a
Massimiliano <m.croci.ly@gmail.com>
2021-01-13 14:11:42 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Creare un layout con i dropdown-menu come da screenshot.
+
+
+$(document).ready(function() {
+
+  // code below
+  $('.show-dropdown').hover(function() {
+    $(this).children('.dropdown-menu').toggleClass('active');
+
+  });
+
+});
